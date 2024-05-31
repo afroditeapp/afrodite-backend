b0c575de2c8f56e6c7b3a1b127abc8a1f82c5da9
Juuso Tuononen <jutuon@outlook.com>
2024-05-31 19:03:19 +0300

    Remove database creation from pre-build script
    Sqlx was removed previously so it is not needed.

--- manager-pre-build-script.sh
+++ manager-pre-build-script.sh
@@ -1,27 +1,8 @@
 #!/bin/bash -eux
 
 # Pre-build script for building the backend with app-manager service.
-# Deletes the current database and creates a new one.
 
 git submodule init
 git submodule update
-
-# Note: Make sure that libsqlite3-dev is installed before running this script.
-
-if ! command -v diesel &> /dev/null; then
-    echo "diesel is not installed. Installing..."
-    cargo install diesel_cli@2.1.0 --no-default-features --features sqlite
-fi
-
-DATABASE_FILE=database/sqlite/current/current.db
-
-mkdir -p database/sqlite/current
-
-if [ -f "$DATABASE_FILE" ]; then
-    echo "Deleting previous database..."
-    rm "$DATABASE_FILE"
-fi
-
-DATABASE_URL="database/sqlite/current/current.db" diesel database reset
 
 echo "Script completed successfully!"
