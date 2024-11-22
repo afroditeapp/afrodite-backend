b401eabe157bbf22eeed091ecc87b0136c0835a8
Juuso Tuononen <jutuon@outlook.com>
2024-11-22 16:20:26 +0200

    Reset push notification tokens on logout

--- crates/server_api_account/src/account/logout.rs
+++ crates/server_api_account/src/account/logout.rs
@@ -2,7 +2,7 @@
 use axum::{extract::State, Extension};
 use model::AccountIdInternal;
 use obfuscate_api_macro::obfuscate_api;
-use server_api::{create_open_api_router, db_write};
+use server_api::{app::ResetPushNotificationTokens, create_open_api_router, db_write};
 use server_data::write::GetWriteCommandsCommon;
 use simple_backend::create_counters;
 use utoipa_axum::router::OpenApiRouter;
@@ -23,7 +23,7 @@ const PATH_POST_LOGOUT: &str = "/account_api/logout";
     ),
     security(("access_token" = [])),
 )]
-pub async fn post_logout<S: WriteData>(
+pub async fn post_logout<S: WriteData + ResetPushNotificationTokens>(
     State(state): State<S>,
     Extension(account_id): Extension<AccountIdInternal>,
 ) -> Result<(), StatusCode> {
@@ -32,11 +32,12 @@ pub async fn post_logout<S: WriteData>(
     db_write!(state, move |cmds| cmds.common().logout(
         account_id,
     ))?;
+    state.reset_push_notification_tokens(account_id).await?;
 
     Ok(())
 }
 
-pub fn logout_router<S: StateBase + WriteData>(s: S) -> OpenApiRouter {
+pub fn logout_router<S: StateBase + WriteData + ResetPushNotificationTokens>(s: S) -> OpenApiRouter {
     create_open_api_router!(
         s,
         post_logout::<S>,
