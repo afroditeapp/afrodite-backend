8106743c19664b4f72710ab38f6be2f4fb6c1936
Juuso Tuononen <jutuon@outlook.com>
2025-01-12 18:38:46 +0200

    Update fcm library

--- crates/server_common/src/lib.rs
+++ crates/server_common/src/lib.rs
@@ -5,12 +5,6 @@
 
 // TODO(prod): Remove webpki-roots dependency
 
-// Ignore warning about unused yup_oauth2.
-// This library depends on yup_oauth2 because
-// fcm library does not have feature flag for
-// using reqwest with rustls and native roots.
-use yup_oauth2 as _;
-
 pub mod app;
 pub mod data;
 pub mod internal_api;
