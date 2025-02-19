441b706909fdc7a6f61bc83a7963321799b6a43c
Juuso Tuononen <jutuon@outlook.com>
2025-02-19 12:27:53 +0200

    Fix API doc

--- crates/server_api/src/lib.rs
+++ crates/server_api/src/lib.rs
@@ -37,6 +37,7 @@ pub mod app {
     components(schemas(
         // Common
         model::common::EventToClient,
+        model::common_admin::ReportIteratorMode,
         // Manager
         manager_model::ScheduledTaskTypeValue,
     )),
