8f00d8f9bd62708d3454ec52087ef215675736cb
Juuso Tuononen <jutuon@outlook.com>
2025-09-08 23:33:33 +0300

    Logout remote bots when remote bot login is disabled

--- crates/server/src/dynamic_config.rs
+++ crates/server/src/dynamic_config.rs
@@ -1,7 +1,11 @@
 use config::file_dynamic::ConfigFileDynamic;
 use error_stack::ResultExt;
-use server_api::app::{GetConfig, WriteDynamicConfig};
+use server_api::{
+    app::{GetAccounts, GetConfig, ReadDynamicConfig, WriteData, WriteDynamicConfig},
+    db_write_raw,
+};
 use server_common::result::Result;
+use server_data::write::GetWriteCommandsCommon;
 use server_state::{
     S,
     dynamic_config::{DynamicConfigEvent, DynamicConfigEventReceiver},
@@ -16,9 +20,11 @@ mod bot;
 #[derive(thiserror::Error, Debug)]
 enum DynamicConfigManagerError {
     #[error("File error")]
-    FileError,
+    File,
     #[error("Bot client error")]
-    BotClientError,
+    BotClient,
+    #[error("Data error")]
+    Data,
 }
 
 /// Drop this when quit starts
@@ -76,6 +82,13 @@ impl DynamicConfigManager {
         mut receiver: DynamicConfigEventReceiver,
         mut quit_notification: ManagerQuitWatcher,
     ) {
+        if !self.state.is_remote_bot_login_enabled() {
+            match self.logout_remote_bots().await {
+                Ok(()) => (),
+                Err(e) => error!("{e:?}"),
+            }
+        }
+
         loop {
             tokio::select! {
                 item = receiver.0.recv() => {
@@ -107,21 +120,22 @@ impl DynamicConfigManager {
 
     async fn update_config(&mut self) -> Result<(), DynamicConfigManagerError> {
         let new_config = ConfigFileDynamic::load_from_current_dir(false)
-            .change_context(DynamicConfigManagerError::FileError)?;
+            .change_context(DynamicConfigManagerError::File)?;
 
-        let restart_bots =
-            self.current_config.backend_config.local_bots != new_config.backend_config.local_bots;
         let load_remote_bot_login_enabled_value =
             self.current_config.backend_config.remote_bot_login
                 != new_config.backend_config.remote_bot_login;
+        let restart_bots =
+            self.current_config.backend_config.local_bots != new_config.backend_config.local_bots;
 
         self.current_config = new_config;
 
+        if load_remote_bot_login_enabled_value {
+            self.load_remote_bot_login_enabled_value_and_logout_remote_bots_if_needed()
+                .await?;
+        }
         if restart_bots {
             self.restart_bots().await?;
-        }
-        if load_remote_bot_login_enabled_value {
-            self.load_remote_bot_login_enabled_value();
         }
 
         Ok(())
@@ -141,7 +155,7 @@ impl DynamicConfigManager {
             if admin || users > 0 {
                 let bots = BotClient::start_bots(self.state.config(), admin, users)
                     .await
-                    .change_context(DynamicConfigManagerError::BotClientError)?;
+                    .change_context(DynamicConfigManagerError::BotClient)?;
                 self.bots = Some(bots);
             }
         }
@@ -149,12 +163,33 @@ impl DynamicConfigManager {
         Ok(())
     }
 
-    fn load_remote_bot_login_enabled_value(&self) {
-        self.state.set_remote_bot_login_enabled(
-            self.current_config
-                .backend_config
-                .remote_bot_login
-                .unwrap_or_default(),
-        );
+    async fn load_remote_bot_login_enabled_value_and_logout_remote_bots_if_needed(
+        &self,
+    ) -> Result<(), DynamicConfigManagerError> {
+        let value = self
+            .current_config
+            .backend_config
+            .remote_bot_login
+            .unwrap_or_default();
+
+        if !value {
+            self.logout_remote_bots().await?
+        }
+
+        self.state.set_remote_bot_login_enabled(value);
+
+        Ok(())
+    }
+
+    async fn logout_remote_bots(&self) -> Result<(), DynamicConfigManagerError> {
+        for b in self.state.config().remote_bots() {
+            if let Some(id) = self.state.get_internal_id_optional(b.account_id()).await {
+                db_write_raw!(self.state, move |cmds| { cmds.common().logout(id).await })
+                    .await
+                    .map_err(|e| e.into_report())
+                    .change_context(DynamicConfigManagerError::Data)?;
+            }
+        }
+        Ok(())
     }
 }
