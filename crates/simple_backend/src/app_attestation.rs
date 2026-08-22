f760c48a4b36c3fd463c748ad1e27cd9f649f8f1
Juuso Tuononen <jutuon@outlook.com>
2026-08-22 12:55:41 +0300

    Simplify app attestation validation logic

--- crates/simple_backend/src/app_attestation.rs
+++ crates/simple_backend/src/app_attestation.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use simple_backend_config::SimpleBackendConfig;
+use simple_backend_config::{SimpleBackendConfig, file::PlayIntegrityAppAttestationConfig};
 use simple_backend_model::{AppAttestation, AppAttestationResult, AppAttestationTypeNumber};
 
 use crate::app_attestation::play_integrity::{PlayIntegrityError, PlayIntegrityManager};
@@ -46,50 +46,58 @@ impl AppAttestationManager {
         client_type: Option<AppAttestationClientType>,
         attestation: Option<&AppAttestation>,
     ) -> std::result::Result<Option<AppAttestationResult>, AppAttestationError> {
-        let Some(config) = self.config.app_attestation() else {
-            return Ok(None);
-        };
-
-        let Some(attestation) = attestation else {
-            return Err(AppAttestationError::Failed);
-        };
-
-        if let Some(play_integrity) = &attestation.play_integrity {
-            // Google Play Integrity API is only supported on Android clients.
-            if client_type != Some(AppAttestationClientType::Android) {
-                return Err(AppAttestationError::Failed);
+        // Skip app attestation validation if server config for the required
+        // app attestation method does not exist.
+        match client_type {
+            Some(AppAttestationClientType::Android) => {
+                if let Some(config) = self.config.app_attestation()
+                    && let Some(play_integrity_config) = &config.play_integrity
+                {
+                    self.handle_play_integrity(play_integrity_config, attestation)
+                        .await
+                } else {
+                    Ok(None)
+                }
             }
-            let Some(play_integrity_config) = &config.play_integrity else {
-                return Err(AppAttestationError::Failed);
-            };
-            let nothing_required = !play_integrity_config.require_device_integrity
-                && !play_integrity_config.require_app_integrity;
+            None => Ok(None),
+        }
+    }
+
+    async fn handle_play_integrity(
+        &self,
+        play_integrity_config: &PlayIntegrityAppAttestationConfig,
+        attestation: Option<&AppAttestation>,
+    ) -> std::result::Result<Option<AppAttestationResult>, AppAttestationError> {
+        let nothing_required = !play_integrity_config.require_device_integrity
+            && !play_integrity_config.require_app_integrity;
+
+        if let Some(attestation) = attestation
+            && let Some(play_integrity) = &attestation.play_integrity
+        {
             match self
                 .play_integrity
                 .validate(play_integrity, play_integrity_config)
                 .await
             {
-                Ok(integrity) => {
-                    return Ok(Some(AppAttestationResult::Success {
-                        attestation_type: AppAttestationTypeNumber::PlayIntegrity,
-                        integrity,
-                    }));
-                }
-                Err(_) if nothing_required => {
-                    return Ok(Some(AppAttestationResult::Failure {
-                        attestation_type: AppAttestationTypeNumber::PlayIntegrity,
-                    }));
-                }
-                Err(error) => {
-                    return Err(match error {
-                        PlayIntegrityError::Failed => AppAttestationError::Failed,
-                        PlayIntegrityError::DeviceIntegrity => AppAttestationError::DeviceIntegrity,
-                        PlayIntegrityError::AppIntegrity => AppAttestationError::AppIntegrity,
-                    });
-                }
+                Ok(integrity) => Ok(Some(AppAttestationResult::Success {
+                    attestation_type: AppAttestationTypeNumber::PlayIntegrity,
+                    integrity,
+                })),
+                Err(_) if nothing_required => Ok(Some(AppAttestationResult::Failure {
+                    attestation_type: AppAttestationTypeNumber::PlayIntegrity,
+                })),
+                Err(error) => Err(match error {
+                    PlayIntegrityError::Failed => AppAttestationError::Failed,
+                    PlayIntegrityError::DeviceIntegrity => AppAttestationError::DeviceIntegrity,
+                    PlayIntegrityError::AppIntegrity => AppAttestationError::AppIntegrity,
+                }),
+            }
+        } else {
+            if nothing_required {
+                Ok(None)
+            } else {
+                Err(AppAttestationError::Failed)
             }
         }
-
-        Err(AppAttestationError::Failed)
     }
 }
